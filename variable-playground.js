8b08414f196bcba8d49a465d3fe3311ad527bfaa
Joseph Harrington <joe@pankowconstruction.com>
2016-11-16 23:10:17 -0700

    Add variable-playground area

---
+++ variable-playground.js
@@ -0,0 +1,23 @@
+// var person = {
+// 	name: 'Andrew',
+// 	age: 21
+// };
+
+// function updatePerson (obj) {
+
+// 	obj.age = 24;
+
+// }
+
+// updatePerson(person);
+// console.log(person);
+
+var grades = [15, 88]
+
+function addGrade (obj) {
+	obj.push(55);
+	debugger;
+}
+
+addGrade(grades);
+console.log(grades);
